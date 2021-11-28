6258ab0ae31d7bcbaa789cea374638eec3d23f11
alexcoder04 <ailyushonak@gmail.com>
2021-11-28 21:52:53 +0100

    fix 32-4 issues

--- src/recv.go
+++ src/recv.go
@@ -11,7 +11,7 @@ import (
 
 func RecvPacket(conn net.Conn) (int, int, HeaderValues, []byte, error) {
 	// receive
-	buffer := make([]byte, 2+3+4+(16*1024*1024)+32+3+(16*1024*1024)+32)
+	buffer := make([]byte, 2+3+(16*1024*1024)+3+(16*1024*1024))
 	_, err := conn.Read(buffer[:])
 	if err != nil {
 		log.Println(err.Error())
@@ -29,19 +29,19 @@ func RecvPacket(conn net.Conn) (int, int, HeaderValues, []byte, error) {
 	command := binary.BigEndian.Uint32(buffer[2+3 : 2+3+4])
 	log.Println("command:", command)
 
-	headerRaw := buffer[2+3+4 : 2+3+4+headerLength]
+	headerRaw := buffer[2+3+4 : 2+3+4+(headerLength-32)]
 	log.Println("header:", hex.EncodeToString(headerRaw))
 
-	headerChecksum := buffer[2+3+4+headerLength : 2+3+4+headerLength+32]
+	headerChecksum := buffer[2+3+(headerLength-32) : 2+3+headerLength]
 	log.Println("header checksum:", hex.EncodeToString(headerChecksum))
 
-	bodyLength := binary.BigEndian.Uint32(buffer[2+3+4+headerLength+32 : 2+3+4+headerLength+32+3])
+	bodyLength := binary.BigEndian.Uint32(buffer[2+3+headerLength : 2+3+headerLength+3])
 	log.Println("body length:", bodyLength)
 
-	body := buffer[2+3+4+headerLength+32+3 : 2+3+4+headerLength+32+3+bodyLength]
+	body := buffer[2+3+headerLength+3 : 2+3+headerLength+3+(bodyLength-32)]
 	log.Println("body:", hex.EncodeToString(body))
 
-	bodyChecksum := buffer[2+3+4+headerLength+32+3+bodyLength : 2+3+4+headerLength+32+3+bodyLength+32]
+	bodyChecksum := buffer[2+3+headerLength+3+(bodyLength-32) : 2+3+headerLength+3+bodyLength]
 	log.Println("body checksum:", bodyChecksum)
 
 	// parse
